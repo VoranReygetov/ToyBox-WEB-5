6c3290c98f4de237b8ffc220c05dd6f8e8b4c94b
VoranReygetov <voran009@gmail.com>
2024-05-14 12:27:11 +0300

    Made a 1 js file from 2 for 1 task

--- task8.js
+++ task8.js
@@ -1,3 +1,32 @@
+function findSmallestCommon() {
+    // Отримання масивів від користувача
+    const array1 = document.getElementById("array1").value.split(",").map(Number);
+    const array2 = document.getElementById("array2").value.split(",").map(Number);
+
+    // Знаходження спільних елементів
+    const commonElements = array1.filter(element => array2.includes(element));
+
+    if (commonElements.length === 0) {
+        document.getElementById("result").innerText = "Немає співпадаючих елементів.";
+    } else {
+        const smallestCommon = Math.min(...commonElements);
+        document.getElementById("result-1").innerText = "Найменший співпадаючий елемент: " + smallestCommon;
+    }
+}
+
+function bubbleSort() {
+    const arr = document.getElementById("array1").value.split(",").map(Number);
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    document.getElementById("result-2").innerText = "Відсортований масив бульбашкою: " + arr;
+}
+
 let alarmTime;
 let alarmSet = false; 
 let alarmInterval; 
